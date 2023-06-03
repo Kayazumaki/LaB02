868ea071025098e5136907f38acd8af1c21b09e2
Kayazumaki <128226948+Kayazumaki@users.noreply.github.com>
2023-06-03 18:21:21 +0200

    Add files via upload

---
+++ Lab02_zad5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double x, y;
+
+        Console.WriteLine("wpisz 2 liczby rzeczywiste");
+        Console.WriteLine("wpisz 1 liczbę");
+        x = inputValue();
+        Console.WriteLine("wpisz 2 liczbę");
+        y = inputValue();
+        Console.WriteLine("liczba większa to:");
+        if (x > y)
+        {
+            Console.WriteLine(x);
+        }
+        else
+        {
+            if (y > x)
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
