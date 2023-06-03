ed09db54b285730ccc04df864a2ea21c95454a78
Kayazumaki <128226948+Kayazumaki@users.noreply.github.com>
2023-06-03 18:25:41 +0200

    Add files via upload

---
+++ Lab02_Zad6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+
+        Console.WriteLine("sprawdź czy trójkąt może być równoboczny");
+        Console.WriteLine("1 bok trójkąta");
+        a = inputValue();
+        Console.WriteLine("2 bok trójkąta");
+        b = inputValue();
+        Console.WriteLine("3 bok trójkąta");
+        c = inputValue();
+        if (a == b)
+        {
+            if (a == c)
+            {
+                Console.WriteLine("TAK! trójkąt może być równoboczny");
+            }
+            else
+            {
+                Console.WriteLine("NIE! trójkąt nie może być równoboczny");
+            }
+        }
+        else
+        {
+            Console.WriteLine("NIE! trójkąt nie może być równoboczny");
+        }
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
